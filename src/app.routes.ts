440766b090f0e56c9f262cafc76725b76501319f
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-10-03 20:21:42 -0600

    Se agregó routeLink con valor "../" para regresar a la página anterior, se agregó Router para la programación pragmática el enviar el formulario y que redirijjiera con la propiedad replaceUrl en true para evitar que use el botón de volver del navegador al envíar un formulario. Se agregó una ruta por defecto para cuando intente acceder a una innexistente mediante una última ruta con el path:="**".

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -3,6 +3,7 @@ import { NoTaskComponent } from "./app/tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./app/users/user-tasks/user-tasks.component";
 import { TaskComponent } from "./app/tasks/task/task.component";
 import { NewTaskComponent } from "./app/tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./app/not-found/not-found.component";
 
 export const routes = [
     {
@@ -22,5 +23,9 @@ export const routes = [
                 component:NewTaskComponent
             }
         ]
+    },
+    {
+        path:'**',
+        component:NotFoundComponent
     }
 ]
